ceb27f297245b34414168ae00dfc3b7beade7497
Luke Austin <luke.austin@ensiie.fr>
2019-05-05 10:52:31 +0200

    added test coverage

---
+++ stryker.conf.js
@@ -0,0 +1,21 @@
+module.exports = function(config) {
+  config.set({
+    mutator: "javascript",
+    mutate: [
+      '!./index.js',
+      '!./src/HttpClient.js',
+      './src/Entity/Jetpack.js ',
+      'src/Service/Api/JetpackApi.js',
+      'src/util.js'
+    ],
+    packageManager: "npm",
+    reporters: ["clear-text", "progress"],
+    testRunner: "jest",
+    transpilers: [],
+    coverageAnalysis: "off",
+    thresholds: {
+      break: 30 
+      // ..
+    }
+  });
+};
